348729c227d81cf627459a0f83b780fc79c032fd
gsom95 <igorsapozhnikov9@gmail.com>
2020-11-26 17:59:06 +0300

    Prepare basic layout
    Add title and picker for choosing conversion mode.
    Work in progress.

--- UnitConverter/UnitConverter/ContentView.swift
+++ UnitConverter/UnitConverter/ContentView.swift
@@ -14,9 +14,31 @@ enum Conversions: String, Equatable, CaseIterable {
 }
 
 struct ContentView: View {
+    let conversionToUnits: [Conversions: [Unit]] = [
+        .temperature: [UnitTemperature.celsius, UnitTemperature.fahrenheit, UnitTemperature.kelvin],
+        .length: [UnitLength.centimeters, UnitLength.meters, UnitLength.kilometers, UnitLength.feet, UnitLength.yards, UnitLength.miles],
+        .volume: [UnitVolume.cubicCentimeters, UnitVolume.cubicMeters, UnitVolume.cubicFeet, UnitVolume.cubicYards],
+    ]
+
+    @State private var currentConversion = Conversions.temperature
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            HStack {
+                Form {
+                    Section(header: Text("Choose what type of conversion you want")) {
+                        Picker("Conversion type", selection: $currentConversion) {
+                            ForEach(Conversions.allCases, id: \.self) { val in
+                                Text(val.rawValue.capitalized)
+                                    .tag(val)
+                            }
+                        }
+                    }
+                    .textCase(.none)
+                }
+            }
+            .navigationBarTitle("Unit converter")
+        }
     }
 }
 
