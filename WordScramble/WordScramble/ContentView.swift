406bf76cb6a41b9b38cd4ec48cc32eff7ddd091a
gsom95 <igorsapozhnikov9@gmail.com>
2020-12-08 20:56:02 +0300

    Add checks and more interaction
    For some reason there is no SwiftUI-y way to keep focus on the textfield (at least as of Dec'20) — you have to do it through UIKit (as I understood). Nevertheless, this is still a fun game.

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -11,6 +11,10 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    @State private var score = 0
 
     var body: some View {
         NavigationView {
@@ -24,9 +28,22 @@ struct ContentView: View {
                     Image(systemName: "\($0.count).circle")
                     Text($0)
                 }
+
+                HStack {
+                    Text("Your score is \(score)")
+                        .font(.callout)
+                    Spacer()
+                }
+                .padding()
             }
             .navigationBarTitle(rootWord)
+            .navigationBarItems(leading: Button(action: startGame, label: {
+                Text("New word")
+            }))
             .onAppear(perform: startGame)
+            .alert(isPresented: $showingError, content: {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("Ok")))
+            })
         }
     }
 
@@ -37,6 +54,9 @@ struct ContentView: View {
             let allWords = startWords.components(separatedBy: "\n")
             rootWord = allWords.randomElement() ?? "silkworm"
 
+            score = 0
+            usedWords.removeAll()
+
             return
         }
 
@@ -44,16 +64,67 @@ struct ContentView: View {
     }
 
     func addNewWord() {
-        let lowerCased = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
 
-        guard lowerCased.count > 0 else {
+        guard answer.count > 2 else {
+            wordError(title: "Word is too short", message: "The word should be at least 3 letters long")
             return
         }
 
-        // validataion here
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word is used already", message: "Be more original!")
+            return
+        }
 
-        usedWords.insert(lowerCased, at: 0)
+        guard isPossible(word: answer) else {
+            wordError(title: "Word is not recognized", message: "Use the letters from the word, y'know.")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word is not possible", message: "That's not a real word.")
+            return
+        }
+
+        usedWords.insert(answer, at: 0)
+        score += answer.count
         newWord = ""
+    }
+
+    // MARK: Check methods
+
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word) || (word != rootWord)
+    }
+
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+
+    // MARK: Show error
+
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
