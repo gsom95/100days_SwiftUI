a9e4f10965c958e216a1d353c659145272db77b3
gsom95 <igorsapozhnikov9@gmail.com>
2020-12-08 16:47:41 +0300

    Create base project

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Igor on 08.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
