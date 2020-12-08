0b62e5c3a84a48b48f7e8a6ab354b2eb7430497e
gsom95 <igorsapozhnikov9@gmail.com>
2020-12-08 17:12:06 +0300

    Create base UI layout

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .autocapitalization(.none)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+            }
+            .navigationBarTitle(rootWord)
+        }
+    }
+
+    func addNewWord() {
+        let lowerCased = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        guard lowerCased.count > 0 else {
+            return
+        }
+
+        // validataion here
+
+        usedWords.insert(lowerCased, at: 0)
+        newWord = ""
     }
 }
 
