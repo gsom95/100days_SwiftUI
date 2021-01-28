c3a570dc27d775a201555e817c9e8b438eb70c32
gsom95 <igorsapozhnikov9@gmail.com>
2021-01-28 17:37:27 +0300

    Customizing animations in SwiftUI
    Change default type of animation to spring type.

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
         .foregroundColor(.white)
         .clipShape(Circle())
         .scaleEffect(animationAmount)
-        .animation(.default)
+        .animation(.interpolatingSpring(stiffness: 50, damping: 3))
         .blur(radius: (animationAmount - 1) * 3)
     }
 }
