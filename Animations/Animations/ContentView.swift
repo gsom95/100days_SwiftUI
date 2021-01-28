b849ad1015d905eb1149cd1d6aa50fd4aa6bc48a
gsom95 <igorsapozhnikov9@gmail.com>
2021-01-28 18:12:25 +0300

    Animating bindings
    We can "bind" bindings with animation so that the state change will invoke animation on the view.

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -11,25 +11,21 @@ struct ContentView: View {
     @State private var animationAmount: CGFloat = 1
 
     var body: some View {
-        Button("Tap me") {
-//            self.animationAmount += 1
-        }
-        .padding(40)
-        .background(Color.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(Color.red)
-                .scaleEffect(animationAmount)
-                .opacity(Double(2 - animationAmount))
-                .animation(
-                    Animation.easeOut(duration: 1)
-                        .repeatForever(autoreverses: false)
-                )
-        )
-        .onAppear {
-            self.animationAmount = 2
+        print(animationAmount)
+
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(), in: 1 ... 10)
+
+            Spacer()
+
+            Button("Tap Me") {
+                self.animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
         }
     }
 }
