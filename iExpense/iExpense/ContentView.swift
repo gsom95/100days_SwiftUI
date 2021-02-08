28245d48709eb72340ea4c10ec1706df34d46e6f
gsom95 <igorsapozhnikov9@gmail.com>
2021-02-08 20:24:17 +0300

    Showing and hiding views
    Create a basic view and show it as sheet (almost like showing an alert).

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,21 +7,22 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    @Published var firstName = "Bilbo"
-    @Published var lastName = "Baggins"
-}
-
 struct ContentView: View {
-    @ObservedObject var user = User()
+    @State private var showingSheet = false
 
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName)")
-
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
+        Button("Show Sheet") {
+            self.showingSheet.toggle()
         }
+        .sheet(isPresented: $showingSheet, content: {
+            SecondView()
+        })
+    }
+}
+
+struct SecondView: View {
+    var body: some View {
+        Text("Second View")
     }
 }
 
