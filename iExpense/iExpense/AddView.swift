7300bafe873ec8a222cbfbd2ed9b3d586dd308b6
gsom95 <igorsapozhnikov9@gmail.com>
2021-03-02 15:59:17 +0300

    Making changes permanent with UserDefaults
    (My initiative) Add dismiss to save, so that a user doesn't need to swipe down after saving expense.

--- iExpense/iExpense/AddView.swift
+++ iExpense/iExpense/AddView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
+
     @State private var name = ""
     @State private var type = "Personal"
     @State private var amount = ""
@@ -33,6 +35,7 @@ struct AddView: View {
                 if let actualAmount = Int(self.amount) {
                     let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
                     self.expenses.items.append(item)
+                    self.presentationMode.wrappedValue.dismiss()
                 }
             })
         }
