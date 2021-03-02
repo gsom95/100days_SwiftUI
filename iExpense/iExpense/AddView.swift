920d6d1a47dedefb7d394b054ae80a5db3f73a69
gsom95 <igorsapozhnikov9@gmail.com>
2021-03-02 15:30:56 +0300

    Sharing an observed object with a new view
    Create new view for adding an expense.

---
+++ iExpense/iExpense/AddView.swift
@@ -0,0 +1,38 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Igor on 02.03.2021.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+
+    static let types = ["Business", "Personal"]
+
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Add new expense")
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView()
+    }
+}
