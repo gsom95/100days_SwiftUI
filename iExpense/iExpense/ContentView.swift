d91fa6a64937ac0963d40ce95458a417239beacf
gsom95 <igorsapozhnikov9@gmail.com>
2021-03-02 15:04:25 +0300

    Building a list we can delete from
    Add Expenses to the view and add some interactions for the list. Currently, items on the list are identified incorrectly (by name property of ExpenseItem): this would be a problem if items have the same name.

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+
     var body: some View {
-        Text("Hello")
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarItems(trailing:
+                Button(action: {
+                    let expense = ExpenseItem(name: "test", type: "Personal", amount: 5)
+                    self.expenses.items.append(expense)
+                }, label: {
+                    Image(systemName: "plus")
+                }))
+            .navigationBarTitle("iExpense")
+        }
+    }
+
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
