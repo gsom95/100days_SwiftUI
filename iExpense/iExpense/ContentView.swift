9415c70cf42655c6659807932bf16021b1157ca4
gsom95 <igorsapozhnikov9@gmail.com>
2021-03-02 13:23:44 +0300

    Revert ContentView to initial state

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,38 +7,9 @@
 
 import SwiftUI
 
-struct User: Codable {
-    var firstName: String
-    var lastName: String
-}
-
 struct ContentView: View {
-    @State private var user = User(firstName: "Taylor", lastName: "Swift")
-
     var body: some View {
-        VStack {
-            Button("Save User") {
-                let encoder = JSONEncoder()
-
-                let newUser = User(firstName: "Dune", lastName: "Emperor")
-
-                if let data = try? encoder.encode(newUser) {
-                    UserDefaults.standard.set(data, forKey: "UserData")
-                }
-            }
-
-            Button("Load User") {
-                let decoder = JSONDecoder()
-
-                if let userData = UserDefaults.standard.data(forKey: "UserData"),
-                   let user = try? decoder.decode(User.self, from: userData)
-                {
-                    self.user = user
-                }
-            }
-
-            Text("\(user.firstName) \(user.lastName)")
-        }
+        Text("Hello")
     }
 }
 
