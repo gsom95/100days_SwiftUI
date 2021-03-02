3e0822583f96056ac1e8f7ec78dc99d457007d04
gsom95 <igorsapozhnikov9@gmail.com>
2021-03-02 13:25:16 +0300

    Building a list we can delete from
    Create a struct that represent a single expense.

---
+++ iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,14 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Igor on 02.03.2021.
+//
+
+import Foundation
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
